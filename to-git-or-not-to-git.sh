484a40cf9a84bb502d4deade14e6a985517d6359
Andreas Rafail Andrikopoulos <aandriko@noreply.platform.zone01.gr>
2024-04-02 19:56:05 +0000

    Upload files to ''

--- to-git-or-not-to-git.sh
+++ to-git-or-not-to-git.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "name": "Chameleon"
-echo "power": 28
-echo "gender": "Male"
+#Display the name, power, and gender of the hero using a here document
+
+curl -s https://platform.zone01.gr/assets/superhero/all.json | jq -r '.[] | select(.id == 170) | .name, .powerstats.power, .appearance.gender' 
