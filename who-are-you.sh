f75ac0936273a1a80a0c102c60f9dedd4860e496
Andreas Rafail Andrikopoulos <aandriko@noreply.platform.zone01.gr>
2024-04-02 21:37:06 +0000

    Upload files to ''

--- who-are-you.sh
+++ who-are-you.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #Display the name of who you are using a here document
 
-curl -s https://platform.zone01.gr/assets/superhero/all.json | jq' .[] | select(.id == 70) | .name'
+curl -s https://platform.zone01.gr/assets/superhero/all.json | jq ' .[] | select(.id == 70) | .name'
+#./who-are-you.sh | cat -e
 
 
