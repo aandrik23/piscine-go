2728c056fe2f917d31f699ef5e108e19c493bae5
aandriko <andreasrafailandrikopoulos@gmail.com>
2024-04-07 15:54:11 +0300

    pointone

--- to-git-or-not-to-git.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#Display the name, power, and gender of the hero using a here document
-
-curl -s https://platform.zone01.gr/assets/superhero/all.json | jq -r '.[] | select(.id == 170) | .name, .powerstats.power, .appearance.gender' 
--- who-are-you.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#Display the name of who you are using a here document
-
-curl -s https://platform.zone01.gr/assets/superhero/all.json | jq ' .[] | select(.id == 70) | .name'
-#./who-are-you.sh | cat -e
-
-
