71e6ffb0adc4f734a6b0e422c89e3a403b216f46
aandriko <andreasrafailandrikopoulos@gmail.com>
2024-04-08 19:56:39 +0300

    pointerone

---
+++ iterativefactorial.go
@@ -0,0 +1,20 @@
+package piscine
+
+// func IterativeFactorial(nb int) int {
+// 	result := 0 {
+
+// 	if nb < 0 {
+// 		return
+// 	}
+// 	result := 1
+
+// 	for i := 1; i <= nb; i++ {
+// 		result *= i
+// 	}
+// 	return 1
+
+// 	if result < 0
+// 	return 0
+//  }
+// return result
+// }
