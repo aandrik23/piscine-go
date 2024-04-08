cae66c5cc8adf20b87961b9dafa9e831882ed30b
aandriko <andreasrafailandrikopoulos@gmail.com>
2024-04-08 20:46:07 +0300

    awd

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,20 +1,18 @@
 package piscine
 
-// func IterativeFactorial(nb int) int {
-// 	result := 0 {
+func IterativeFactorial(nb int) int {
+	if nb < 0 {
+		return 0
+	}
+	result := 1
 
-// 	if nb < 0 {
-// 		return
-// 	}
-// 	result := 1
+	for i := 1; i <= nb; i++ {
+		result *= i
 
-// 	for i := 1; i <= nb; i++ {
-// 		result *= i
-// 	}
-// 	return 1
+		if result < 0 {
+			return 0
+		}
 
-// 	if result < 0
-// 	return 0
-//  }
-// return result
-// }
+	}
+	return result
+}
