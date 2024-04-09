fba2dc8a23458c87a914cd92da250b85ef1e0ddc
aandriko <andreasrafailandrikopoulos@gmail.com>
2024-04-09 19:27:43 +0300

    fibonacci

---
+++ fibonacci.go
@@ -0,0 +1,14 @@
+package piscine
+
+func Fibonacci(index int) int {
+	for index < 0 {
+		return -1
+	}
+	for index == 0 {
+		return 0
+	}
+	for index == 1 {
+		return 1
+	}
+	return (Fibonacci(index-2) + Fibonacci(index-1))
+}
