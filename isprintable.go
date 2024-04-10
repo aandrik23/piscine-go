f53ace4e71db89627063b86b81cf42e4ad748a03
aandriko <andreasrafailandrikopoulos@gmail.com>
2024-04-10 19:41:27 +0300

    isprintable

--- isprintable.go
+++ isprintable.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsPrintable(s string) bool {
 	for _, i := range []rune(s) {
-		if (i < 'a' || i > 'z') && (i < 'A' || i > 'Z') {
+		if (i < 'A' || i > 'Z') && (i < 'a' || i > 'z') && (i == '\n') {
 			return false
 		}
 	}
