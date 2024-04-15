bcdbf9cc192ff259bbe6467dd949e32fd2863b29
aandriko <andreasrafailandrikopoulos@gmail.com>
2024-04-15 22:17:37 +0300

    boolean

---
+++ boolean/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func printStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
+}
+
+func isEven(nbr int) bool {
+	if nbr%2 == 1 {
+		return false
+	} else {
+		return true
+	}
+}
+
+func main() {
+	if isEven(lengthOfArg(os.Args[1:])) {
+		printStr("I have an even number of arguments")
+	} else {
+		printStr("I have an odd number of arguments")
+	}
+}
+func lengthOfArg(arr []string) int {
+	lens := 0
+	for range arr {
+		lens++
+	}
+	return lens
+}
