0f3ced528cb2260c6cf572f0dbb3622d6dd0d474
aandriko <andreasrafailandrikopoulos@gmail.com>
2024-04-16 23:06:17 +0300

    issorted

---
+++ issorted.go
@@ -0,0 +1,37 @@
+package piscine
+
+func f(a, b int) int {
+	if a < b {
+		return -1
+	} else if a > b {
+		return 1
+	}
+	return 0
+}
+
+func IsSorted(f func(a, b int) int, a []int) bool {
+	if len(a) < 2 {
+		return true
+	}
+	sortedAsc := false
+	sortedDesc := false
+	for i := 1; i < len(a); i++ {
+		if f(a[i-1], a[i]) > 0 {
+			sortedAsc = true
+			if sortedDesc {
+				return false
+			}
+		}
+		if f(a[i-1], a[i]) < 0 {
+			sortedDesc = true
+			if sortedAsc {
+				return false
+			}
+		}
+		if f(a[i-1], a[i]) == 0 {
+			sortedDesc = true
+			sortedAsc = true
+		}
+	}
+	return sortedAsc || sortedDesc
+}
