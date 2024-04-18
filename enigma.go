61c56fb3668e9e3fa3219fe907f3c4e7f0c0df06
aandriko <andreasrafailandrikopoulos@gmail.com>
2024-04-18 22:50:51 +0300

    enigma

---
+++ enigma.go
@@ -0,0 +1,13 @@
+package piscine
+
+func Enigma(a ***int, b *int, c *******int, d ****int) {
+	tempA := ***a
+	tempB := *b
+	tempC := *******c
+	tempD := ****d
+
+	***a = tempC
+	*******c = tempD
+	****d =tempB
+	*b = tempA
+	}
