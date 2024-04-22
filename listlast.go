418fce6ef8b8558725c8ec0b2b3c718371875588
aandriko <andreasrafailandrikopoulos@gmail.com>
2024-04-22 20:48:01 +0300

    list last

--- listlast.go
+++ listlast.go
@@ -10,12 +10,9 @@ package piscine
 // 	Tail *NodeL
 // }
 
-func ListSize(l *List) int {
-	size := 0
-	current := l.Head
-	for current != nil {
-		size++
-		current = current.Next
+func ListLast(l *List) interface{} {
+	if l.Tail != nil {
+		return l.Tail.Data
 	}
-	return size
+	return nil
 }
