f37e206b9752cd1b5aa78513b7c2cc3abc6047c7
aandriko <andreasrafailandrikopoulos@gmail.com>
2024-04-22 20:05:56 +0300

    listpushfront

--- listpushfront.go
+++ listpushfront.go
@@ -10,15 +10,15 @@ type List struct {
 	Tail *NodeL
 }
 
-func ListPushBack(l *List, data interface{}) {
-	n := &NodeL{Data: data}
+func ListPushFront(l *List, data interface{}) {
+	n := &NodeL{Data:data}
 
-	if l.Head == nil && l.Tail == nil {
+	if l.Head == nil {
 		l.Head = n
 		l.Tail = n
 
 	} else {
-		l.Tail.Next = n
-		l.Tail = n
+		n.Next = l.Head
+		l.Head = n
 	}
 }
