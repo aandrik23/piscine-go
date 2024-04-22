db2d6e648a2bf2885c2b9d8ce8dd00fada296f69
aandriko <andreasrafailandrikopoulos@gmail.com>
2024-04-22 20:49:55 +0300

    list last

--- listpushback.go
+++ listpushback.go
@@ -1,23 +1,23 @@
 package piscine
 
 type NodeL struct {
-    Data interface{}
-    Next *NodeL
+        Data interface{}
+        Next *NodeL
 }
 
 type List struct {
-    Head *NodeL
-    Tail *NodeL
+        Head *NodeL
+        Tail *NodeL
 }
 
 func ListPushBack(l *List, data interface{}) {
-    n := &NodeL{Data: data} 
-    if l.Head == nil && l.Tail == nil {
-        l.Head = n
-        l.Tail = n
-        } else {
+        n := &NodeL{Data: data} 
+        if l.Head == nil && l.Tail == nil {
+            l.Head = n
+            l.Tail = n
+            } else {
 
-        l.Tail.Next = n 
-        l.Tail = n      
-    }
+            l.Tail.Next = n 
+            l.Tail = n      
+        }
 }
