52a790dd3e620c7ffe5c63e63fb905e56bbf9a28
aandriko <andreasrafailandrikopoulos@gmail.com>
2024-04-25 20:41:26 +0300

    ultim

--- printcomb2.go
+++ printcomb2.go
@@ -3,10 +3,10 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb2() {
-	for a := '0'; a <= '9'; a++{
+	for a := '0'; a <= '9'; a++ {
 		for b := '0'; b <= '8'; b++ {
 			for c := '0'; c <= '9'; c++ {
-				for d := '0'; d <= '9'; d++{
+				for d := '0'; d <= '9'; d++ {
 					if a == '9' && b == '8' && c == '9' && d == '9' {
 						z01.PrintRune(a)
 						z01.PrintRune(b)
