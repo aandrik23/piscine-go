954d04702a6d9753f5bbbe2a4b03ac480e59306e
aandriko <andreasrafailandrikopoulos@gmail.com>
2024-08-27 22:10:10 +0300

    printcomb.

--- printcomb.go
+++ printcomb.go
@@ -6,7 +6,7 @@ func PrintComb() {
 	for a := '0'; a <= '9'; a++ {
 		for b := '0'; b <= '9'; b++ {
 			for c := '0'; c <= '9'; c++ {
-				if a < b && b < c {
+				if a < b && b < c && a != '7' {
 					z01.PrintRune(a)
 					z01.PrintRune(b)
 					z01.PrintRune(c)
