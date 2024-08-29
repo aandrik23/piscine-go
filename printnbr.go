7020c383809ad94d2282cbf0aa15c24da8775da7
aandriko <andreasrafailandrikopoulos@gmail.com>
2024-08-29 21:05:32 +0300

    printnbr.

--- printnbr.go
+++ printnbr.go
@@ -39,6 +39,13 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintNbr(n int) {
+	if n == -9223372036854775808 {
+		str := "-9223372036854775808"
+		for _, ch := range str {
+			z01.PrintRune(ch)
+		}
+		return
+	}
 	if n < 0 {
 		z01.PrintRune('-')
 		n = -n
