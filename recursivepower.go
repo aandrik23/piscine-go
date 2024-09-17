255ec985ad73584f8c48cbc97abef8bfed478e2d
aandriko <andreasrafailandrikopoulos@gmail.com>
2024-09-17 19:56:19 +0300

    recpower.

--- recursivepower.go
+++ recursivepower.go
@@ -1,13 +1,12 @@
 package piscine
 
 func RecursivePower(nb int, power int) int {
-	if power < 0 {
+	if power > 0 {
 		return 0
 	}
 	if power == 0 {
 		return 1
 	}
-	// result := 1
 	if power == 1 {
 		return nb
 	}
